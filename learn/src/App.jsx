b634a561f046c9af3cf353d10481d1f633efd8b0
choigirang <choigirang@kakao.com>
2023-01-24 16:01:41 +0900

    chap.6_update

--- learn/src/App.jsx
+++ learn/src/App.jsx
@@ -1,140 +1,253 @@
 // commit test
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
-function Header(props){
-  return <header>
-    <h1><a href="/" onClick={(event)=>{
-      event.preventDefault();
-      props.onChangeMode();
-    }}>{props.title}</a></h1>
-  </header>
-
-
+function Header(props) {
+  return (
+    <header>
+      <h1>
+        <a
+          href="/"
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode();
+          }}
+        >
+          {props.title}
+        </a>
+      </h1>
+    </header>
+  );
 }
-function Nav(props){
-  const lis = []
-  for(let i=0; i< props.topics.length; i++){
-    let t = props.topics[i]
-    lis.push(<li key={t.id}><a id={t.id} href={'/read/'+t.id} onClick={(event)=>{
-      event.preventDefault();
-      props.onChangeMode(Number(event.target.id));
-    }}>{t.title}</a></li>)
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(
+      <li key={t.id}>
+        <a
+          id={t.id}
+          href={"/read/" + t.id}
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode(Number(event.target.id));
+          }}
+        >
+          {t.title}
+        </a>
+      </li>
+    );
   }
   return (
     <nav>
-        <ol>
-          {lis}
-        </ol>
-      </nav>
-  )
+      <ol>{lis}</ol>
+    </nav>
+  );
 }
 
-function Article(props){
-  return(
+function Article(props) {
+  return (
     <article>
-        <h2>{props.title}</h2>
-      </article>
-  )
+      <h2>{props.title}</h2>
+    </article>
+  );
 }
 
-function Create(props){
-  return <article>
-    <h2>Create</h2>
-    <form onSubmit={event=>{
-      event.preventDefault();
-      // submit은 클릭을 했을 때 페이지가 리로드 된다.
-      // 이것을 막는다.
-      const title = event.target.title.value
-      // submit 버튼을 눌렀을 때 발생하는 event는 form 태그를 가르키기 때문에
-      // name이 title인 태그의 value를 가져온다.
-      const body = event.target.body.value;
-      // title,body 질문
-      props.onCreate(title,body);
-    }}>
-      {/* 정보를 전송하기 위한 form */}
-      <p><input type="text" name='title' placeholder='title'/></p>
-      <p><textarea name="body" placeholder='body'></textarea></p>
-      <p><input type="submit" value="create"/></p>
-    </form>
-  </article>
+function Create(props) {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onCreate(title, body);
+        }}
+      >
+        <p>
+          <input type="text" name="title" placeholder="title" />
+        </p>
+        <p>
+          <textarea name="body" placeholder="body"></textarea>
+        </p>
+        <p>
+          <input type="submit" value="create" />
+        </p>
+      </form>
+    </article>
+  );
 }
 
-function App(){
-  const [mode, setMode] = useState('WELCOME')
-  const [id, setId] = useState(null)
+function Update(props) {
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+  return (
+    <article>
+      <h2>Update</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onUpdate(title, body);
+        }}
+      >
+        <p>
+          <input
+            type="text"
+            name="title"
+            placeholder="title"
+            value={title}
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+          />
+        </p>
+        {/* 그냥 value 값을 props.title로 줬을 때는 값이 변하지 않는다.
+      이 props는 우리가 지정해주어 변하지 않기 때문에 props 또한
+      변할 수 있는 상태로 만들어줘야 한다. useState */}
+        {/* onChange는 html에서 마우스가 바깥으로 나갈 때...작동하지만
+      React에서는 값을 입력할 때마다 발생한다. */}
+        <p>
+          <textarea
+            name="body"
+            placeholder="body"
+            value={body}
+            onChange={(event) => {
+              setBody(event.target.value);
+            }}
+          ></textarea>
+        </p>
+        <p>
+          <input type="submit" value="Update" />
+        </p>
+      </form>
+    </article>
+  );
+}
+
+function App() {
+  const [mode, setMode] = useState("WELCOME");
+  const [id, setId] = useState(null);
   const [topics, setTopics] = useState([
-    // Creat를 통해 input을 하고, input한 value 값인 title과 body를
-    // topics안에다가 넣어야 하기 때문에 topics도 상태로 만들어
-    // 새로운 값을 가질 수 있도록 한다.
-    {id:1, title:'html', body:'html is ...'},
-    {id:2, title:'css', body:'css is ...'},
-    {id:3, title:'java', body:'java is ...'}
+    { id: 1, title: "html", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+    { id: 3, title: "java", body: "java is ..." },
   ]);
   const [nextId, setNextId] = useState(4);
-  // Create를 통해 객체의 title과 body는 추가되지만 id는 추가되지 않는다.
-  // 새롭게 추가되는 topics 객체에 id를 추가하는 방법은 여러가지가 있겠지만
-  // id만 따라 빼고 값이 변하는 상태를 만들기 위함, 이미 앞에 1,2,3의 id가
-  // 있기 때문에 초기값은 4부터 시작하면 된다.
-
   let content = null;
-  if(mode === 'WELCOME'){
-    content = <Article title="Welcome" body="Hello,WEB"></Article>
-  }else if(mode === 'READ'){
-    let title, body = null;
-    for(let i=0; i<topics.length; i++){
-      if(topics[i].id === id){
+  let contextControl = null;
+
+  if (mode === "WELCOME") {
+    content = <Article title="Welcome" body="Hello,WEB"></Article>;
+  } else if (mode === "READ") {
+    let title,
+      body = null;
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) {
         title = topics[i].title;
         body = topics[i].body;
       }
     }
-    content = <Article title={title} body={body}></Article>
-  }else if(mode === 'CREAT'){
-    content = <Create onCreate={(_title,_body)=>{
-      const newTopic = {id:nextId, title:_title, body:_body}
-      const newTopics = [...topics]
-      // 상태가 string,boolean,number ... 등의 원시자료라면 그래도 사용하면 되지만
-      // 상태가 object,array 같이 참조자료라면 이것을 복제한 후 상태를 변경해야 한다.
-      // 우리가 topics안에 추가하려 하는데 이때, topics는 객체이기 떄문에
-      // newTopics를 만들어 복제를 해주어야한다.
-      newTopics.push(newTopic);
-      setTopics(newTopics)
-      setMode('READ');
-      setId(nextId);
-      setNextId(nextId++);
-      // 추가된 id를 계속 추가시키고, 추가된 내용을 볼 수 있으며
-      // creat input 창이 닫아지도록 READ로 바꿔준다.
-    }}></Create>
+    content = <Article title={title} body={body}></Article>;
+    contextControl = (
+      <li>
+        <a
+          href={"/update/" + id}
+          onClick={(event) => {
+            event.preventDefault();
+            setMode("UPDATE");
+          }}
+        >
+          Update
+        </a>
+      </li>
+    );
+    // mode가 READ일 때만 contextControl이 나타나게 한다.
+  } else if (mode === "CREAT") {
+    content = (
+      <Create
+        onCreate={(_title, _body) => {
+          const newTopic = { id: nextId, title: _title, body: _body };
+          const newTopics = [...topics];
+          newTopics.push(newTopic);
+          setTopics(newTopics);
+          setMode("READ");
+          setId(nextId);
+          setNextId(nextId++);
+        }}
+      ></Create>
+    );
+  } else if (mode === "UPDATE") {
+    let title,
+      body = null;
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = (
+      <Update
+        title={title}
+        body={body}
+        onUpdate={(title, body) => {
+          // READ일 때, title과 body값을 알아냈던 것과 같다.
+          const newTopics = [...topics];
+          const updatedTopic = { id: id, title: title, body: body };
+          // READ일 때, Update를 통해 입력한 title과 body 값이
+          // 새로운 topic을 구성하며 id는 READ일 때를 그대로 받아온다.
+          for (let i = 0; i < newTopics.length; i++) {
+            if (newTopics[i].id === id) {
+              newTopics[i] = updatedTopic;
+              // newTopics의 id와 updatedTopic의 id가 일치하는 것은
+              // 우리가 클릭한 id를 일컫는다.
+              break;
+              // 일치하면 종료
+            }
+          }
+          setTopics(newTopics);
+          setMode("READ");
+        }}
+      ></Update>
+    );
   }
-  // 예) const [value, setValue] = useState([1]);
-  // 배열을 가진 value에서
-  // value.push(2) - value의 오리지널 값을 바꾼것이다.
-  // setValue(value) - 상태는 값의 변화가 없으면 굳이 컴포넌트를
-  // 다시 렌더링 하지 않는데, 우리는 오리지널 값을 그대로 추가한 것이기 때문에
-  // 상태 변화에 따른 재실행이 일어나지 않는다.
-  // 따라서, newValue = [...value] - 오리지널 데이터를 복제하고
-  // newValue.push(2) - 추가하고
-  // setValue(newValue) 로 상태변화.
 
-
-  return(
+  return (
     <div>
-      <Header title="REACT" onChangeMode={()=>{
-        setMode('WELCOME');
-      }}></Header>
-      <Nav topics={topics} onChangeMode={(_id)=>{
-        setMode('READ');
-        setId(_id)
-      }}></Nav>
+      <Header
+        title="REACT"
+        onChangeMode={() => {
+          setMode("WELCOME");
+        }}
+      ></Header>
+      <Nav
+        topics={topics}
+        onChangeMode={(_id) => {
+          setMode("READ");
+          setId(_id);
+        }}
+      ></Nav>
       {content}
-      <a href="/create" onClick={event=>{
-        event.preventDefault();
-        setMode('CREATE')
-        {/* setMode의 상태가 Create가 되었을 때*/}
-      }}></a>
+
+      <ul>
+        <li>
+          <a
+            href="/create"
+            onClick={(event) => {
+              event.preventDefault();
+              setMode("CREATE");
+            }}
+          >
+            Create
+          </a>
+        </li>
+        {contextControl}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App;
